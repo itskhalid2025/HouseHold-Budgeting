b61eb7bf588519891bcaaef169b55b790c2693bd
itskhalid2025 <itskhalid.25@gmail.com>
2026-02-07 15:50:46 +0530

    backusse2

--- backend/server.js
+++ backend/server.js
@@ -126,7 +126,7 @@ const frontendPath = path.join(__dirname, '../../frontend/dist');
 app.use(express.static(frontendPath));
 
 // Handle React Router deep links
-app.get('(.*)', (req, res, next) => {
+app.get('/:path*', (req, res, next) => {
     if (req.path.startsWith('/api/')) return next();
     res.sendFile(path.join(frontendPath, 'index.html'), (err) => {
         if (err) next();
